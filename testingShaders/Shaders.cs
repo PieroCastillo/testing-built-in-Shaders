ea4a8381b07ce213e3d362bfd94745794c9ae34e
Piero Castillo <pierocastillo2005@gmail.com>
2021-08-26 23:46:57 -0500

    Agregar archivos de proyecto.

---
+++ testingShaders/Shaders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testingShaders
+{
+    public static class Shaders
+    {
+        public static string vertex = 
+@"
+#version 450
+layout(location = 0) in vec3 inPos;
+layout(location = 1) uniform Transform
+{
+    mat3x2 model;
+    mat4 view;
+    mat4 projection;
+} t;
+
+void main()
+{
+    gl_Position = vec4(inPos, 1.0) * t.model * t.view * t.projection;
+}
+";
+        public static string fragment =
+@"
+#version 450
+layout(location = 0) in vec4 color;
+layout(location = 0) out vec4 outColor;
+
+void main()
+{
+    outColor = color;
+}
+";
+    }
+}
---
+++ testingShaders/sampleApp.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Vortice.ShaderCompiler;
+using Vortice.Vulkan;
+using Vultaik;
+using Vultaik.Desktop;
+
+namespace testingShaders
+{
+    public unsafe class SampleApp
+    {
+        Window window;
+        Device device;
+        SwapChain swapChain;
+        GraphicsContext context;
+        Framebuffer framebuffer;
+
+        GraphicsPipeline pipeline;
+        
+        public SampleApp()
+        {
+            Init();
+            CreateBuffers();
+            CreatePipeline();
+                        
+            window.Show();
+            window.RenderLoop(OnRender);
+        }
+
+        void Init()
+        {
+            window = new Window("sample", 800, 600);
+
+            var adapterconfig = new AdapterConfig()
+            {
+                SwapChain = true
+            };
+            var adapter = new Adapter(adapterconfig);
+
+            device = new Device(adapter);
+            swapChain = new(device, new()
+            {
+                Source = GetSwapchainSource(window, adapter),
+                Width = 800,
+                Height = 600,
+                ColorSrgb = false,
+                DepthFormat = adapter.DepthFormat is VkFormat.Undefined ? null : adapter.DepthFormat
+            });
+
+            context = new(device);
+            framebuffer = new(swapChain);
+        }
+
+        void CreatePipeline()
+        {
+            var compiler = new Compiler();
+
+            var vertex = compiler.Compile(Shaders.vertex, string.Empty, ShaderKind.VertexShader).GetBytecode().ToArray();
+            var frag = compiler.Compile(Shaders.fragment, string.Empty, ShaderKind.FragmentShader).GetBytecode().ToArray();
+
+            GraphicsPipelineDescription pd = new();
+            pd.SetFramebuffer(framebuffer);
+            pd.SetShader(new ShaderBytecode(vertex, ShaderStage.Vertex, ShaderBackend.Glsl));
+            pd.SetShader(new ShaderBytecode(frag, ShaderStage.Fragment, ShaderBackend.Glsl));
+            pd.SetVertexBinding(VkVertexInputRate.Vertex, VertexPositionColor.Size);
+            pd.AddVertexAttribute<VertexPositionColor>();
+            pipeline = new(pd);
+        }
+
+        void CreateBuffers()
+        {
+
+        }
+
+        void OnRender()
+        {
+            Console.WriteLine("c renderiza");
+        }
+
+        public SwapchainSource GetSwapchainSource(Window window, Adapter adapter)
+        {
+            if (adapter.SupportsSurface)
+            {
+                if (adapter.SupportsWin32Surface)
+                    return window.SwapchainWin32!;
+
+                if (adapter.SupportsX11Surface)
+                    return window.SwapchainX11!;
+
+                if (adapter.SupportsWaylandSurface)
+                    return window.SwapchainWayland!;
+
+                if (adapter.SupportsMacOSSurface)
+                    return window.SwapchainNS!;
+            }
+
+            throw new PlatformNotSupportedException("Cannot create a SwapchainSource.");
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Transform2D
+    {
+        public Matrix3x2 model;
+        public Matrix4x4 view;
+        public Matrix4x4 projection;
+    }
+}
